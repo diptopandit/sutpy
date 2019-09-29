b1ef1ee8e5a6dde52ce11853b7c97b56cbf1c0c1
Dipto Pandit (dipandit) <dipandit@cisco.com>
2019-09-30 03:28:24 +0530

    fixed format.

--- ut/Test.py
+++ ut/Test.py
@@ -1,19 +1,21 @@
-import time
 from TestCase import TestCase
-#from OutputChannel import OutputChannel
 
-class bcolors:
+
+# from OutputChannel import OutputChannel
+
+
+class BColors:
     HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
+    OK_BLUE = '\033[94m'
+    OK_GREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
-    ENDC = '\033[0m'
+    END_C = '\033[0m'
     BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+
 
 class Test:
-    def __init__(self,tid,verbose=False):
+    def __init__(self, tid, verbose=False):
         self.testId = tid
         self.testCases = list()
         self.requestedCases = list()
@@ -24,82 +26,86 @@ class Test:
         self.passedCases = 0
         self.maxTime = 0
         self.ready = False
-        self.error = "ERROR::"+self.testId+":No command specified.\nERROR::"+self.testId+":No test case available.\n"
+        self.error = "ERROR::" + self.testId + ":No command specified.\nERROR::" + self.testId + ":No test case available.\n"
         self.verbose = verbose
-        self.commandstr = ""
-        #self.outchannel = OutChannel()
-        #self.out = self.outchannel.out
+        self.command_str = ""
+        # self.outchannel = OutChannel()
+        # self.out = self.outchannel.out
 
-    def execute(self,reqCases=[],skip=[]):
-        if(self.commandstr):
+    def execute(self, req_cases=[], skip=[]):
+        if self.command_str:
             if self.totalCases == 0:
-                self.error = "ERROR::"+self.testId+":No test case available.\n"
+                self.error = "ERROR::" + self.testId + ":No test case available.\n"
                 return False
 
-            if not reqCases :
+            if not req_cases:
                 self.requestedCases = self.testCases
                 self.numberOfRequestedCases = self.totalCases
             else:
-                for case in reqCases:
-                    self.requestedCases.append(self.testCases[case-1])
+                for case in req_cases:
+                    self.requestedCases.append(self.testCases[case - 1])
                     self.numberOfRequestedCases = self.numberOfRequestedCases + 1
             if skip:
                 for case in skip:
-                    if self.testCases[case-1] in self.requestedCases:
-                        self.requestedCases.remove(self.testCases[case-1])
+                    if self.testCases[case - 1] in self.requestedCases:
+                        self.requestedCases.remove(self.testCases[case - 1])
                         self.numberOfRequestedCases = self.numberOfRequestedCases - 1
 
-            #self.out(OutChannel.SPLOUT,"Running test : %s | %d test cases",self.testId,self.totalCases)
-            #self.out(OutChannel.STDOUT,"==========================================================")
-            print "Running test : %s | %d test cases" %(self.testId,self.numberOfRequestedCases)
-            print "=========================================================="
+            # self.out(OutChannel.SPLOUT,"Running test : %s | %d test cases",self.testId,self.totalCases)
+            # self.out(OutChannel.STDOUT,"==========================================================")
+            print("Running test : %s | %d test cases" % (self.testId, self.numberOfRequestedCases))
+            print("==========================================================")
             for testCase in self.requestedCases:
-                testCase.execute(self.commandstr)
+                testCase.execute(self.command_str)
                 if not testCase.result:
                     self.result = False
                 self.elapsedTime += testCase.elapsedTime
-                self.maxTime = max(self.maxTime,testCase.elapsedTime)
+                self.maxTime = max(self.maxTime, testCase.elapsedTime)
                 if testCase.result:
                     self.passedCases += 1
-            print "Test %s Completed" %(self.testId)
-            print "=========================================================="
-            print "Passed: %d, Failed %d (%d%% success)" %(self.passedCases, (self.numberOfRequestedCases - self.passedCases), ((self.passedCases*100)/self.numberOfRequestedCases))
-            print "Test result: "+(bcolors.OKGREEN+"PASSED"+bcolors.ENDC if self.result else bcolors.FAIL+"FAILED" +bcolors.ENDC)+", Elapsed time: %.3f sec" %(self.elapsedTime)
-            print "Max time: %.3f sec, Avarage time: %.3f sec" %(self.maxTime,self.elapsedTime/self.numberOfRequestedCases)
-            print "=========================================================="
+            print("Test " + self.testId + " Completed")
+            print("==========================================================")
+            print("Passed: %d, Failed %d (%d%% success)" % (self.passedCases,
+                                                            (self.numberOfRequestedCases - self.passedCases),
+                                                            ((self.passedCases * 100) / self.numberOfRequestedCases)))
+            print("Test result: " +
+                  (BColors.OK_GREEN + "PASSED" + BColors.END_C if self.result else BColors.FAIL + "FAILED" +
+                   BColors.END_C) + ", Elapsed time: %.3f sec" % self.elapsedTime)
+            print("Max time: %.3f sec, Average time: %.3f sec" % (
+                self.maxTime, self.elapsedTime / self.numberOfRequestedCases))
+            print("==========================================================")
             return self.result
         else:
-            self.error = "ERROR::"+self.testId+": No command specified.\n"
+            self.error = "ERROR::" + self.testId + ": No command specified.\n"
             return False
 
-    def testFile(self,tcFile):
-        testCaseId = 0
+    def test_file(self, tc_file):
+        test_case_id = 0
         try:
-            with open(tcFile) as fp:
+            with open(tc_file) as fp:
                 for line in fp:
                     line = line.strip()
                     if (not line) or (line[0] is '#'):
                         continue
-                    testCaseId = testCaseId + 1
+                    test_case_id = test_case_id + 1
                     exp = fp.next()
-                    tc = TestCase(testCaseId,line.strip(),exp.strip())
-                    tc.verbose = self.verbose 
+                    tc = TestCase(test_case_id, line.strip(), exp.strip())
+                    tc.verbose = self.verbose
                     self.testCases.append(tc)
                     self.totalCases += 1
         except StopIteration:
-            print "WARN::"+options.testcases+": Incomplete testcase"
-        if not (self.commandstr):
+            print("WARN::" + tc_file + ": Incomplete test case")
+        if not self.command_str:
             self.ready = False
-            self.error = "ERROR::"+self.testId+":No command specified.\n"
+            self.error = "ERROR::" + self.testId + ":No command specified.\n"
         else:
             self.ready = True
-        return (len(self.testCases) == self.totalCases)
+        return len(self.testCases) == self.totalCases
 
-    def command(self,cmd):
-        self.commandstr = cmd
+    def command(self, cmd):
+        self.command_str = cmd
         if self.totalCases != 0:
             self.ready = True
         else:
-            self.error = "ERROR::"+self.testId+":No test case available.\n"
+            self.error = "ERROR::" + self.testId + ":No test case available.\n"
             self.ready = False
-
--- ut/TestCase.py
+++ ut/TestCase.py
@@ -1,16 +1,17 @@
 import time
 import subprocess
-#from OutputChannel import OutputChannel
+# from OutputChannel import OutputChannel
 
-class bcolors:
+
+class BColors:
     HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
+    OK_BLUE = '\033[94m'
+    OK_GREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
-    ENDC = '\033[0m'
+    END_C = '\033[0m'
     BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+
 
 try:
     from subprocess import STDOUT, check_output, CalledProcessError                                                                                                                          
@@ -20,19 +21,19 @@ except ImportError:  # pragma: no cover
     import subprocess
     STDOUT = subprocess.STDOUT
 
-    def check_output(*popenargs, **kwargs):
+    def check_output(*p_open_args, **kwargs):
         if 'stdout' in kwargs:  # pragma: no cover
             raise ValueError('stdout argument not allowed, '
                              'it will be overridden.')
         process = subprocess.Popen(stdout=subprocess.PIPE,
-                                   *popenargs, **kwargs)
+                                   *p_open_args, **kwargs)
         output, _ = process.communicate()
-        retcode = process.poll()
-        if retcode:
+        return_code = process.poll()
+        if return_code:
             cmd = kwargs.get("args")
             if cmd is None:
-                cmd = popenargs[0]
-            raise subprocess.CalledProcessError(retcode, cmd,
+                cmd = p_open_args[0]
+            raise subprocess.CalledProcessError(return_code, cmd,
                                                 output=output)
         return output
     subprocess.check_output = check_output
@@ -41,20 +42,19 @@ except ImportError:  # pragma: no cover
     # keyword not being available (in 2.6)
     class CalledProcessError(Exception):
 
-        def __init__(self, returncode, cmd, output=None):
-            self.returncode = returncode
+        def __init__(self, return_code, cmd, output=None):
+            self.m_return_code = return_code
             self.cmd = cmd
             self.output = output
 
         def __str__(self):
             return "Command '%s' returned non-zero exit status %d" % (
-                self.cmd, self.returncode)
+                self.cmd, self.m_return_code)
     subprocess.CalledProcessError = CalledProcessError
 
 
-
 class TestCase:
-    def __init__(self,cid,inp, exp):
+    def __init__(self, cid, inp, exp):
         self.caseId = cid
         self.name = "TC#"+str(cid)
         self.inputStr = inp
@@ -62,23 +62,24 @@ class TestCase:
         self.result = False
         self.verbose = False
         self.elapsedTime = 0
+        self.actual = 0
 
-    def execute(self,command):
+    def execute(self, command):
         if self.verbose:
-            print bcolors.HEADER+"Executing "+self.name+":"+bcolors.ENDC 
-            print "input   : "+self.inputStr
-            print "expected: "+self.expected
+            print(BColors.HEADER+"Executing " + self.name + ":" + BColors.END_C)
+            print("input   : " + self.inputStr)
+            print("expected: " + self.expected)
         try:
             start_time = time.time()
-            self.actual = subprocess.check_output([command,self.inputStr]).strip()
+            self.actual = subprocess.check_output([command, self.inputStr]).strip()
             self.result = (self.expected == self.actual)
             self.elapsedTime = time.time() - start_time
             if self.verbose:
-                color = bcolors.OKGREEN if self.result else bcolors.FAIL
-                print "actual  : "+color+self.actual+bcolors.ENDC
-                print "result  : "+color+str(self.result)+bcolors.ENDC+" Elapsed time: %.3f sec" % (self.elapsedTime)
+                color = BColors.OK_GREEN if self.result else BColors.FAIL
+                print("actual  : "+color+self.actual+BColors.END_C)
+                print("result  : "+color+str(self.result)+BColors.END_C+" Elapsed time: %.3f sec" % self.elapsedTime)
         except Exception as e:
-            print bcolors.WARNING+"ERROR::"+self.name+": "+str(e)+bcolors.ENDC
+            print(BColors.WARNING+"ERROR::"+self.name+": "+str(e)+BColors.END_C)
         if self.verbose:
-            print "----------------------------------------------------------"
+            print("----------------------------------------------------------")
 
